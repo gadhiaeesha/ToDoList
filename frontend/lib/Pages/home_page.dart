eac71c26a9569fac69669fec624cc508f460d976
gadhiaeesha <gadhiaeesha@gmail.com>
2024-03-27 07:33:55 -0700

    i tried, sometimes it works and sometimes it doesnt, oh well

--- frontend/lib/Pages/home_page.dart
+++ frontend/lib/Pages/home_page.dart
@@ -1,132 +1,152 @@
-// ignore_for_file: avoid_print
-
 import 'package:flutter/material.dart';
 import 'package:frontend/Constants/api.dart';
 import 'package:frontend/Models/todo.dart';
 import 'package:frontend/Widgets/todo_container.dart';
 import 'package:http/http.dart' as http;
+import 'package:pie_chart/pie_chart.dart';
 import 'dart:convert';
 import 'package:frontend/Widgets/app_bar.dart';
-import 'package:pie_chart/pie_chart.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
+  HomePage({Key? key}) : super(key: key);
 
   @override
-  // ignore: library_private_types_in_public_api
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
   int done = 0;
-  
   List<Todo> myTodos = [];
   bool isLoading = true;
 
-  void _showModal() {
+  void _showModel() {
+    String title ="";
+    String desc = "";
     showModalBottomSheet(
-            context: context, 
-            builder: (BuildContext context) { //context is variable which stores information of current state
-              return Container(
-                height: MediaQuery.of(context).size.height / 2,
-                color: Colors.grey,
-                child: const Center(
-                  child: Column(
-                    children: [
-                      Padding(
-                        padding: EdgeInsets.all(10.0),
-                        child: Text(
-                          "Add new Item",
-                          style: TextStyle(
-                            fontSize: 25,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.black,
-                          ),
-                        ),
-                      ),
-                      SizedBox(height: 10),
-                      Padding(
-                        padding: EdgeInsets.all(20.0),
-                        child: TextField(
-                          obscureText: true,
-                          decoration: InputDecoration(
-                            border: OutlineInputBorder(),
-                            labelText: "Title",
-                          )
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.all(20.0),
-                        child: TextField(
-                          obscureText: true,
-                          decoration: InputDecoration(
-                            border: OutlineInputBorder(),
-                            labelText: "Description",
-                          )
-                        ),
-                      ),
-                      SizedBox(height: 10),
-                      ElevatedButton(
-                        onPressed: null,
-                        child: Padding(
-                          padding: EdgeInsets.all(10.0),
-                          child: Text(
-                            "Add",
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 20,
-                            )
-                          ),
-                        )
-                      ),
-                    ],
+      context: context,
+      builder: (BuildContext context) {
+        return Container(
+          height: MediaQuery.of(context).size.height / 2,
+          child: Center(
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  "Add your Todo",
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
-              );
-            },
-          );
+                SizedBox(
+                  height: 10,
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Title',
+                  ),
+                  onSubmitted: (value) {
+                    setState(() {
+                      title = value;
+                    });
+                  },
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Description',
+                    
+                  ),
+                  onSubmitted: (value) {
+                    setState(() {
+                      desc = value;
+                    });
+                  },
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                ElevatedButton(
+                  onPressed: () =>  _postData(
+                    title: title,
+                    desc: desc
+                  ),
+                  child: Text("Add"),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
   }
 
   void fetchData() async {
-    try{
+    try {
       http.Response response = await http.get(Uri.parse(api));
       var data = json.decode(response.body);
       data.forEach((todo) {
         Todo t = Todo(
-          id: todo["id"],
+          id: todo['id'],
           title: todo['title'],
           desc: todo['desc'],
           isDone: todo['isDone'],
           date: todo['date'],
         );
-
-        if(todo['isDone']){
+        if (todo['isDone']) {
           done += 1;
         }
         myTodos.add(t);
       });
-
       print(myTodos.length);
       setState(() {
         isLoading = false;
       });
-    }
-    catch(e) {
+    } catch (e) {
       print("Error is $e");
     }
   }
 
-  // ignore: non_constant_identifier_names
-  void delete_todo(String id) async {
-    try{
-      // ignore: unused_local_variable
-      http.Response response = await http.delete(Uri.parse("$api/$id")); //same as Uri.parse(api + "/" + id)
-      setState(() { //calls the build method [line 74] again
-        myTodos = [];
-      }); 
-      fetchData();
+  void _postData({String title = "", String desc = ""}) async {
+    try {
+      http.Response response = await http.post(
+        Uri.parse(api),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: jsonEncode(<String, dynamic>{
+          "title": title,
+          "desc": desc,
+          "isDone": false,
+        }),
+      );
+      if (response.statusCode == 201) {
+        setState(() {
+          myTodos = [];
+        });
+        fetchData();
+      } else {
+        print("Something went wrong");
+      }
+    } catch (e) {
+      print("Error is $e");
     }
-    catch(e) {
+  }
+
+  void delete_todo(String id) async {
+    try {
+      http.Response response = await http.delete(Uri.parse(api + "/" + id));
+      setState(() {
+        myTodos = [];
+      });
+      fetchData();
+    } catch (e) {
       print(e);
     }
   }
@@ -140,37 +160,39 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFF001133),
+      backgroundColor: Color(0xFF001133),
       appBar: customAppBar(),
       body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
         child: Column(
           children: [
             PieChart(
               dataMap: {
                 "Done": done.toDouble(),
-                "Incomplete": (myTodos.length - done).toDouble(),
-              }
+                "Imcomplete": (myTodos.length - done).toDouble(),
+              },
             ),
-            isLoading ? const CircularProgressIndicator() : 
-            Column(
-              children: myTodos.map((e) {
-                return TodoContainer(
-                  onPress: () => delete_todo(e.id.toString()),
-                  id: e.id, 
-                  title: e.title, 
-                  desc: e.desc, 
-                  isDone: e.isDone,
-                );
-              }).toList(),
-            )
-          ]
+            isLoading
+                ? CircularProgressIndicator()
+                : Column(
+                    children: myTodos.map((e) {
+                      return TodoContainer(
+                        id: e.id,
+                        onPress: () => delete_todo(e.id.toString()),
+                        title: e.title,
+                        desc: e.desc,
+                        isDone: e.isDone,
+                      );
+                    }).toList(),
+                  )
+          ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          _showModal();
+          _showModel();
         },
-        child: const Icon(Icons.add)
+        child: Icon(Icons.add),
       ),
     );
   }
