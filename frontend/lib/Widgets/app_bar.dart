176754392c376d55f00125e7d248568e67485ce5
gadhiaeesha <gadhiaeesha@gmail.com>
2024-03-27 04:16:07 -0700

    changing appbar text color to white

--- frontend/lib/Widgets/app_bar.dart
+++ frontend/lib/Widgets/app_bar.dart
@@ -2,7 +2,12 @@ import 'package:flutter/material.dart';
 
 AppBar customAppBar() {
   return AppBar(
-    title: const Text("Django Todos"),
+    title: const Text(
+      "Django Todos",
+      style: TextStyle(
+        color: Colors.white,
+      ),
+    ),
     elevation: 0.0,
     backgroundColor: const Color(0xFF001133),
   );
